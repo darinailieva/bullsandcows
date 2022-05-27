074907781382954aaa8dd242d00b4d1e85deec7c
Darina Ilieva <darina.ilieva@gmail.com>
2022-05-27 11:23:53 +0300

    DuplicateEntityException added.

---
+++ BullsAndCows/src/main/java/com/brainstars/bullsandcows/exceptions/DuplicateEntityException.java
@@ -0,0 +1,12 @@
+package com.brainstars.bullsandcows.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DuplicateEntityException extends RuntimeException {
+    public DuplicateEntityException(String type, String attribute, String value) {
+        super(String.format("%s with %s '%s' already exists.", type, attribute, value));
+    }
+}
+
