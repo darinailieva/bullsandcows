dcc9b2934832bff955b0dc580159f41cd5f152e8
Darina Ilieva <darina.ilieva@gmail.com>
2022-05-27 11:22:40 +0300

    Added configuration

--- BullsAndCows/src/main/java/com/brainstars/bullsandcows/BullsAndCowsApplication.java
+++ BullsAndCows/src/main/java/com/brainstars/bullsandcows/BullsAndCowsApplication.java
@@ -2,8 +2,10 @@ package com.brainstars.bullsandcows;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class BullsAndCowsApplication {
 
     public static void main(String[] args) {
